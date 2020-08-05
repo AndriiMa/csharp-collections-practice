581c67ea82a291043e7d81921d8c3dca072c1b97
Andrew Maslyuk <andriima@tahometer.com>
2020-08-05 11:38:48 +0300

    "Added String splitting and counting words logic in Program.cs"

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_collections_practice
+{
+    class Program
+    
+        static void Main(string[] args)
+        {
+
+            String text = "Hello my name is Andrew. The name of my cat is Asya";
+
+            String[] words = text.Replace(".", "").ToLower().Split(" ");
+
+            ItemCounter<String> wordCounter = new ItemCounter<String>();
+
+            Dictionary<String, int> wordTable =  wordCounter.CountItems(words);
+
+            foreach( KeyValuePair<String, int> kvp in wordTable){
+                Console.WriteLine("Word: {0} \tCount: {1}", kvp.Key, kvp.Value);
+            }
+
+        }
+    }
+}
